b3c9d226c9910945138eda7607ed744038b2d198
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-12-19 16:03:51 +0100

    fixing files path

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 """
 this module contains a function that plot a
 red graph
+- x values are in range [0, 10]
+- y values are in range [np.arange(0, 11) ** 3]
 """
 
 
 def line():
     """
     plot a red solid graph with x range [0, 10]
+    this function does not take arguments
+    returns nothing
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
