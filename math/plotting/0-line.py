0e41dba92689fa241acfd58d8f64303383928256
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-12-19 16:06:52 +0100

    fixing files path

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -17,6 +17,6 @@ def line():
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(y, color="red", linestyle="-")
+    plt.plot(y, color='red', linestyle='-')
     plt.xlim(0, 10)
     plt.show()
