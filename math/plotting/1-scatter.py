80bdb31f88897f3def19c59194a2130b00ca9f44
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-12-20 14:54:21 +0100

    scatter

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module contains a scatter function
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    This function (scatter) takes no arguments and returns nothing
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x, y, marker="o", color="magenta", s=5)
+
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (in)")
+    plt.title("Men's Height vs Weight")
+
+    plt.show()
