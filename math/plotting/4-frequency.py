39396b4d94e9230875ae632f3a86b8ea67caf263
ArseneGiriteka <giritekaaarsene@gmail.com>
2025-01-03 15:09:56 +0100

    frequency shown in a histogram chart

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,6 +16,7 @@ def frequency():
     plt.ylabel("Number of Students")
     plt.title("Project A")
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.hist(student_grades, bins=range(0, 110, 10), edgecolor="black")
 
     plt.show()
