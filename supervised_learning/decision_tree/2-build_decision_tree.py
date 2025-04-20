883a815e6712286a874e8834afd55293a580794b
ArseneGiriteka <giritekaaarsene@gmail.com>
2025-04-20 12:50:39 +0200

    printing out decision trees

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -50,7 +50,7 @@ class Node:
         """This method print the left child"""
         lines = text.split("\n")
         new_text = "    +--" + lines[0] + "\n"
-        for x in lines[1:]:
+        for x in lines[1:-1]:
             new_text += ("    |  " + x) + "\n"
         return (new_text)
 
